2582902afdd8e18928d19256b3578fd921385c70
nawzaysfinah <nawzaysfinah@gmail.com>
2023-05-16 10:10:29 +0800

    created 3 x JS support modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-// Testing out webpack initial setup of index.js
-console.log("testing index.js webpack");
+import { createToDo } from "./create-to-do.js";
+import { blankProjectLoad } from "./blank-project-load.js";
+import { initialdomManip } from "./initial-dom-manip.js";
 
-// Testing out DOM manip via webpack
-const contentDiv = document.querySelector(".content");
-const testH1 = document.createElement("h1");
-testH1.textContent = "HELLO WORLD... TEST FROM WEBPACK INDEX.JS DOM MANIP";
-contentDiv.appendChild(testH1);
+// Call blankProjectLoad on first land
+blankProjectLoad();
+
+// Call DOM Manipulation module to control UI
+initialdomManip();
+
+// TODO: click event module HERE for todo and project creations
+
+// Call create-to-do.js module file and apply some objects/properties
+const myToDo = createToDo(
+  "Grocery Run",
+  "Go get groceries",
+  "6/15/2022",
+  "Low",
+  "Meat,"
+);
+const myToDo2 = createToDo(
+  "Homework",
+  "Do Odin Project Work",
+  "6/20/2022",
+  "Medium",
+  "something"
+);
+console.log("Show me the properties on myToDo from index.js ...", myToDo);
+console.log("Show me the properties on myToDo2 from index.js ...", myToDo2);
